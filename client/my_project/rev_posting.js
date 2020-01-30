2dd7186d24afa24205ab863fa73a890789dba1b3
hhz2000 <hhz2000@naver.com>
2020-01-30 17:24:56 +0900

    DB항목추가랑 포스팅 페이지연결 완료

--- client/my_project/rev_posting.js
+++ client/my_project/rev_posting.js
@@ -2,11 +2,11 @@ FlowRouter.template('/rev_posting/:_id', 'rev_posting');
 
 
 Template.rev_posting.onRendered(function() {
-    // $('#editor').summernote({
-    //     popover: {},
-    //     minHeight: 200,
-    //     maximumImageFileSize: 1048576*10
-    // });
+    $('#editor').summernote({
+        popover: {},
+        minHeight: 200,
+        maximumImageFileSize: 1048576*10
+    });
 });
 
 Template.rev_posting.helpers({
@@ -16,44 +16,58 @@ Template.rev_posting.helpers({
             return {};    //새글 작성일때는 화면에 비어있는 데이터를 제공.
         }
 
-        // Meteor.setTimeout(function() { //화면 에디터에 편집 모드를 초기화 하기 위한 트릭
-        //     $('#editor').summernote('reset')
-        // });
+        Meteor.setTimeout(function() { //화면 에디터에 편집 모드를 초기화 하기 위한 트릭
+            $('#editor').summernote('reset')
+        });
 
-        return DB_POSTS.findOne({_id: _id});
+        return DB_REVS.findOne({_id: _id});
     }
 });
 
 Template.rev_posting.events({
     'click #btn-save': function() {
-        // var name = $('#inp-name').val();
+
         var title = $('#inp-title').val();
         // var html = $('#editor').summernote('code');
+        var content=$('#inp-content').val();
+        var place=$('.inp-place').val();
+        var tag=$('.inp-tag').val();
 
         if(!title) {
             return alert('제목은 반드시 입력 해 주세요.');
         }
         var _id = FlowRouter.getParam('_id');
         if( _id === 'newPosting') {
-            DB_POSTS.insert({
+            DB_REVS.insert({
                 createdAt: new Date(),
-                // name: name,
                 title: title,
-                content: html,
-                readCount: 0
+                content: content,
+                place:place,
+                tag:tag,
+                recommend:0,
+                readCount: 0,
+                file_id:[]
+
             })
         } else {
             var post = DB_POSTS.findOne({_id: _id});
-            post.name = name;
+
             post.title = title;
-            post.content = html;
-            DB_POSTS.update({_id: _id}, post);
+            post.content = content;
+            post.place=place;
+            post.tag=tag;
+            DB_REVS.update({_id: _id}, post);
         }
 
         alert('저장하였습니다.');
         // $('#inp-name').val('');
         $('#inp-title').val('');
-        $('#editor').summernote('reset');
+        // $('#editor').summernote('reset');
+        $('#inp-content').val();
+        $('.inp-place').val();
+        $('.inp-tag').val();
+
+
         window.history.back();
     },
 })
--- database.js
+++ database.js
@@ -1,2 +1,3 @@
 // DB = new Mongo.Collection('db');   //데이터베이스 객체 생성 예제
+// DB_REVS = new Mongo.Collection('DB_REVS');
 DB_REVS = new Mongo.Collection('DB_REVS');
