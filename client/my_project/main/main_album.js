dc42a13ee810e0c369c267ec568441cbe975dccd
yoonho0922 <ahn3330@naver.com>
2020-02-07 01:12:56 +0900

    메인 피드 디비 필터링

--- client/my_project/main/main_album.js
+++ client/my_project/main/main_album.js
@@ -6,7 +6,36 @@ Template.main_album.onRendered(function() {
 
 Template.main_album.helpers({
     boards: function() {
-        return DB_REVS.findAll({}, {sort: {createdAt: -1}});
+        var area = FlowRouter.getParam('area');
+        var tag = FlowRouter.getParam('tag');
+        var order = FlowRouter.getParam('order')
+        if(area=='전체'&&tag=='전체'){
+            if(order=='rec'){
+                return DB_REVS.findAll({}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({}, {sort: {createdAt: -1}});
+            }
+        }else if(area=='전체'){
+            if(order=='rec'){
+                return DB_REVS.findAll({'posting_tag': tag}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({'posting_tag': tag}, {sort: {createdAt: -1}});
+            }
+
+        }else if(tag=='전체'){
+            if(order=='rec'){
+                return DB_REVS.findAll({'posting_area': area}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({'posting_area': area}, {sort: {createdAt: -1}});
+            }
+        }else{
+            if(order=='rec'){
+                return DB_REVS.findAll({'posting_area': area, 'posting_tag': tag}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({'posting_area': area, 'posting_tag': tag}, {sort: {createdAt: -1}});
+            }
+        }
+
     },
     YMD: function() {
         return this.createdAt.toStringYMD();
