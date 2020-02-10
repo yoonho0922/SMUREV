09821d2be11085f6b5d458be9a394db4424c0012
yoonho0922 <ahn3330@naver.com>
2020-02-10 21:04:31 +0900

    내가쓴글, 추천한글

--- client/my_project/rev_main.js
+++ client/my_project/rev_main.js
@@ -6,6 +6,7 @@ FlowRouter.template('/rev_main', 'rev_main');
 Template.rev_main.onRendered(function() {
     // 화면이 그려지고 난 후 제일 먼저 수행
     // Session.set('type_list', true);
+
 });
 
 Template.rev_main.helpers({
@@ -25,6 +26,7 @@ Template.rev_main.helpers({
         // if(!tag){
         //     location.href="/rev_main/전체/전체/new";
         // }
+
         return tag;
     }
 });
@@ -46,6 +48,7 @@ Template.rev_main.events({
     
     //태그 버튼 함수
     'click #btn-tag0': function(){
+
     },
     'click #btn-tag1': function(){
     },
--- client/my_project/rev_myPosts.js
+++ client/my_project/rev_myPosts.js
@@ -1,1 +1,73 @@
-FlowRouter.template('/rev_myPosts','rev_myPosts')
+FlowRouter.template('/rev_myPosts/:area/:tag/:order','rev_myPosts')
+
+Template.rev_myPosts.helpers({
+    boards: function() {
+        var area = FlowRouter.getParam('area');
+        var tag = FlowRouter.getParam('tag');
+        var order = FlowRouter.getParam('order')
+        if(area=='전체'&&tag=='전체'){
+            if(order=='rec'){
+                return DB_REVS.findAll({}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({}, {sort: {createdAt: -1}});
+            }
+        }else if(area=='전체'){
+            if(order=='rec'){
+                return DB_REVS.findAll({'posting_tag': tag}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({'posting_tag': tag}, {sort: {createdAt: -1}});
+            }
+
+        }else if(tag=='전체'){
+            if(order=='rec'){
+                return DB_REVS.findAll({'posting_area': area}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({'posting_area': area}, {sort: {createdAt: -1}});
+            }
+        }else{
+            if(order=='rec'){
+                return DB_REVS.findAll({'posting_area': area, 'posting_tag': tag}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({'posting_area': area, 'posting_tag': tag}, {sort: {createdAt: -1}});
+            }
+        }
+
+    },
+    YMD: function() {
+        return this.createdAt.toStringYMD();
+    },
+    HMS: function() {
+        return this.createdAt.toStringHMS();
+    }
+
+});
+
+Template.rev_myPosts.helpers({
+
+    area : function(){
+        var area = FlowRouter.getParam('area');
+        if(!area){
+            return '전체';
+        }
+        return area;
+    },
+    tag : function(){
+        var tag = FlowRouter.getParam('tag');
+        if(!tag){
+            return '전체';
+        }
+        return tag;
+    },
+    order : function(){
+        var order = FlowRouter.getParam('order');
+        if(!order){
+            return 'new';
+        }
+        return order;
+    },
+    link: function() {
+        // 저장 된 이미지 링크를 반환
+        return DB_FILES.findOne({_id: this.file_id}).link()
+
+    }
+});
