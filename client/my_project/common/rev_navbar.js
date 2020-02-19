1350ca4cec46ba78315d5e20ee3b8fe47694263e
hhz2000 <hhz2000@naver.com>
2020-02-19 22:15:42 +0900

    검색기능

--- client/my_project/common/rev_navbar.js
+++ client/my_project/common/rev_navbar.js
@@ -7,5 +7,29 @@ Template.rev_navbar.events({
     'click #btn-logout': function() {
         Meteor.logout();
         alert("로그아웃 되었습니다.");
+    },
+//////검색기능
+    'click #btn-search': function () {
+        alert("검색");
+        var search_content=$('#inp-search').val();
+
+        DB_SEARCH.insert({    //DB에 검색기록 저장
+            search_content:search_content
+        });
+        $('#inp-search').val('');
+        // var _id = DB_SEARCH.findOne({_id: _id});
+        // var _id = FlowRouter.getParam('_id');
+
+
     }
+
 })
+
+Template.rev_navbar.helpers({
+    search_content: function() {
+        return FlowRouter.getParam('search_content');
+    },
+    // search_content: function() {
+    //     return DB_SEARCH.search_content[0];
+    // },
+});
---
+++ client/my_project/rev_searchresult.js
@@ -0,0 +1,133 @@
+FlowRouter.template('/rev_searchresult/:area/:tag/:order','rev_searchresult')
+
+Template.rev_searchresult.helpers({
+    boards: function() {
+
+        var area = FlowRouter.getParam('area');
+        var tag = FlowRouter.getParam('tag');
+        var order = FlowRouter.getParam('order');
+        var search_content=FlowRouter.getParam('search_content')
+        if(area=='전체'&&tag=='전체'){
+            if(order=='rec'){
+                return DB_REVS.findAll({'content':search_content}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({'content':search_content}, {sort: {createdAt: -1}});
+            }
+        }else if(area=='전체'){
+            if(order=='rec'){
+                return DB_REVS.findAll({'content':search_content,'posting_tag': tag}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({'content':search_content,'posting_tag': tag}, {sort: {createdAt: -1}});
+            }
+
+        }else if(tag=='전체'){
+            if(order=='rec'){
+                return DB_REVS.findAll({'content':search_content,'posting_area': area}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({'content':search_content,'posting_area': area}, {sort: {createdAt: -1}});
+            }
+        }else{
+            if(order=='rec'){
+                return DB_REVS.findAll({'content':search_content,'posting_area': area, 'posting_tag': tag}, {sort: {recommend: -1}});
+            }else{
+                return DB_REVS.findAll({'content':search_content,'posting_area': area, 'posting_tag': tag}, {sort: {createdAt: -1}});
+            }
+        }
+
+
+    },
+    YMD: function() {
+        return this.createdAt.toStringYMD();
+    },
+    HMS: function() {
+        return this.createdAt.toStringHMS();
+    }
+
+});
+
+
+Template.rev_searchresult.helpers({
+
+    area : function(){
+        var area = FlowRouter.getParam('area');
+        if(!area){
+            return '전체';
+        }
+        return area;
+    },
+    tag : function(){
+        var tag = FlowRouter.getParam('tag');
+        if(!tag){
+            return '전체';
+        }
+        return tag;
+    },
+    order : function(){
+        var order = FlowRouter.getParam('order');
+        if(!order){
+            return 'new';
+        }
+        return order;
+    },
+    link: function() {
+        // 저장 된 이미지 링크를 반환
+        return DB_FILES.findOne({_id: this.file_id}).link()
+
+    },
+
+    //버튼 활성화 비활성화
+    activated0: function(){
+        if(FlowRouter.getParam('tag') == '전체'){
+            return 'background-color : skyblue; color : white;';
+        }
+        return;
+    },
+    activated1: function(){
+        if(FlowRouter.getParam('tag') == '한식'){
+            return 'background-color : skyblue; color : white;';
+        }
+        return;
+    },
+    activated2: function(){
+        if(FlowRouter.getParam('tag') == '일식'){
+            return 'background-color : skyblue; color : white;';
+        }
+        return;
+    },
+    activated3: function(){
+        if(FlowRouter.getParam('tag') == '중식'){
+            return 'background-color : skyblue; color : white;';
+        }
+        return;
+    },
+    activated4: function(){
+        if(FlowRouter.getParam('tag') == '양식'){
+            return 'background-color : skyblue; color : white;';
+        }
+        return;
+    },
+    activated5: function(){
+        if(FlowRouter.getParam('tag') == '분식'){
+            return 'background-color : skyblue; color : white;';
+        }
+        return;
+    },
+    activated6: function(){
+        if(FlowRouter.getParam('tag') == '카페'){
+            return 'background-color : skyblue; color : white;';
+        }
+        return;
+    },
+    activated7: function(){
+        if(FlowRouter.getParam('tag') == '포차'){
+            return 'background-color : skyblue; color : white;';
+        }
+        return;
+    },
+    activated8: function(){
+        if(FlowRouter.getParam('tag') == '기타'){
+            return 'background-color : skyblue; color : white;';
+        }
+        return;
+    }
+});
